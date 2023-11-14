fd31f167a7209d58f348f974e04ff396d09f8dd5
david <luterancik.d@gmail.com>
2023-11-14 21:14:37 +0100

    Init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { Provider } from 'react-redux'
+import { store } from './store.ts'
+
+const theme = createTheme({
+  palette: {
+      background: {
+          default: '#f3f3f3',
+      },
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <CssBaseline>
+                    <App />
+                </CssBaseline>
+            </Provider>
+        </ThemeProvider>
+    </React.StrictMode>
 )
